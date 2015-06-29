3ea90266c46cfb3da377dfc9d1b9b23aaf2e9762
oliverlangmo <oliver.langmo@gmail.com>
2015-06-29 17:11:20 -0500

    madlibs

---
+++ script.js
@@ -0,0 +1,22 @@
+var thisApp = angular.module('demo', [])
+
+thisApp.controller('demoCtrl', function($scope) {
+	$scope.female_name = '';
+	$scope.adjective = '';
+	$scope.time_of_day='';
+	$scope.destination='';
+	$scope.location1='';
+	$scope.time_of_day='';
+	$scope.destination2='';
+	$scope.occupation='';
+	$scope.adjective2='';
+    $scope.alcohol='';
+    $scope.adjective3='';  
+
+
+
+
+
+
+
+})
